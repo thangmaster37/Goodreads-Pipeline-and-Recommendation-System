131f9a9bc8b44df42188a60e27bd5ed2f100c645
thangmaster37 <nguyenvanthangk66@gmail.com>
2024-05-20 09:16:40 +0700

    first commit

---
+++ airflow/dags/goodreads_etl_pipeline.py
@@ -0,0 +1,190 @@
+from datetime import datetime, timedelta
+import sys
+import subprocess
+
+sys.path.append('../plugins/operators/')
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.providers.ssh.hooks.ssh import SSHHook
+
+# from airflow.contrib.hooks import SSHHook
+# from airflow.contrib.operators.ssh_operator import SSHOperator
+
+# from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.empty import EmptyOperator
+
+# from airflow.hooks.postgres_hook import PostgresHook
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+# from airflow.operators.goodreads_plugin import DataQualityOperator
+
+# from airflow.operators.goodreads_plugin import LoadAnalyticsOperator
+
+from operators.data_quality import DataQualityOperator
+
+from operators.goodreads_analytics import LoadAnalyticsOperator
+
+sys.path.append('../plugins/helpers/')
+
+from helpers.analytics_queries import AnalyticsQueries
+
+sys.path.append('../src')
+
+# from goodreads_driver import main
+
+#config = configparser.ConfigParser()
+#config.read_file(open(f"{Path(__file__).parents[0]}/emr_config.cfg"))
+
+def execute_command():
+    python_file = r'c:\Users\thang\Desktop\Data_Engineer_Project\src\goodreads_driver.py'  # Đường dẫn đến file Python của bạn
+    
+    # Gọi lệnh python để chạy file Python
+    subprocess.call(['python', '-u', python_file])
+
+
+default_args = {
+    'owner': 'goodreads',
+    'depends_on_past': True,
+    'start_date' : datetime(2024, 5, 15, 0, 0, 0, 0),
+    'end_date' : datetime(2024, 5, 17, 0, 0, 0, 0),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    #'retries': 1,
+    #'retry_delay': timedelta(minutes=15),
+    'catchup': True
+}
+
+
+
+dag_name = 'goodreads_pipeline'
+dag = DAG(dag_name,
+          default_args=default_args,
+          description='Load and Transform data from landing zone to processed zone. Populate data from Processed zone to goodreads Warehouse.',
+          #schedule_interval=None,
+          schedule_interval='@daily',
+          max_active_runs = 10
+        )
+
+start_operator = EmptyOperator(task_id='Begin_execution',  dag=dag)
+
+# emrsshHook= SSHHook(ssh_conn_id='emr_ssh_connection')
+
+# jobOperator = SSHOperator(
+#     task_id="GoodReadsETLJob",
+#     command='cd /mnt/c/Users/thang/Desktop/Data_Engineer_Project/src; export PYSPARK_DRIVER_PYTHON=python3; export PYSPARK_PYTHON=python3; spark-submit --master yarn goodreads_driver.py;',
+#     ssh_hook=emrsshHook,
+#     dag=dag)
+
+
+jobOperator = PythonOperator(
+    task_id='GoodReadsETLJob',
+    python_callable=main,
+    dag=dag
+)
+
+warehouse_data_quality_checks = DataQualityOperator(
+    task_id='Warehouse_data_quality_checks',
+    dag=dag,
+    redshift_conn_id = "redshift",
+    tables = ["goodreads_warehouse.authors", "goodreads_warehouse.reviews", "goodreads_warehouse.books", "goodreads_warehouse.users"]
+
+)
+
+
+create_analytics_schema = LoadAnalyticsOperator(
+    task_id='Create_analytics_schema',
+    redshift_conn_id = 'redshift',
+    sql_query = [AnalyticsQueries.create_schema],
+    dag=dag
+)
+
+create_author_analytics_table = LoadAnalyticsOperator(
+    task_id='Create_author_analytics_table',
+    redshift_conn_id = 'redshift',
+    sql_query = [AnalyticsQueries.create_author_reviews,AnalyticsQueries.create_author_rating, AnalyticsQueries.create_best_authors],
+    dag=dag
+)
+
+create_book_analytics_table = LoadAnalyticsOperator(
+    task_id='Create_book_analytics_table',
+    redshift_conn_id = 'redshift',
+    sql_query = [AnalyticsQueries.create_book_reviews,AnalyticsQueries.create_book_rating, AnalyticsQueries.create_best_books],
+    dag=dag
+)
+
+# Authors Analytics Tasks
+
+load_author_table_reviews = LoadAnalyticsOperator(
+    task_id='Load_author_table_reviews',
+    redshift_conn_id = 'redshift',
+    sql_query = [AnalyticsQueries.populate_authors_reviews.format('2024-05-01 00:00:00.000000', '2024-05-31 00:00:00.000000')],
+    dag=dag
+)
+
+
+load_author_table_ratings = LoadAnalyticsOperator(
+    task_id='Load_author_table_ratings',
+    redshift_conn_id = 'redshift',
+    sql_query = [AnalyticsQueries.populate_authors_ratings.format('2024-05-01 00:00:00.000000', '2024-05-31 00:00:00.000000')],
+    dag=dag
+)
+
+load_best_author = LoadAnalyticsOperator(
+    task_id='Load_best_author',
+    redshift_conn_id = 'redshift',
+    sql_query = [AnalyticsQueries.populate_best_authors],
+    dag=dag
+)
+
+
+# Book Analytics Tasks
+load_book_table_reviews = LoadAnalyticsOperator(
+    task_id='Load_book_table_reviews',
+    redshift_conn_id = 'redshift',
+    sql_query = [AnalyticsQueries.populate_books_reviews.format('2024-05-01 00:00:00.000000', '2024-05-31 00:00:00.000000')],
+    dag=dag
+)
+
+
+load_book_table_ratings = LoadAnalyticsOperator(
+    task_id='Load_book_table_ratings',
+    redshift_conn_id = 'redshift',
+    sql_query = [AnalyticsQueries.populate_books_ratings.format('2024-05-01 00:00:00.000000', '2024-05-31 00:00:00.000000')],
+    dag=dag
+)
+
+load_best_book = LoadAnalyticsOperator(
+    task_id='Load_best_books',
+    redshift_conn_id = 'redshift',
+    sql_query = [AnalyticsQueries.populate_best_books],
+    dag=dag
+)
+
+
+
+authors_data_quality_checks = DataQualityOperator(
+    task_id='Authors_data_quality_checks',
+    dag=dag,
+    redshift_conn_id = "redshift",
+    tables = ["goodreads_analytics.popular_authors_average_rating", "goodreads_analytics.popular_authors_average_rating"]
+
+)
+
+books_data_quality_checks = DataQualityOperator(
+    task_id='Books_data_quality_checks',
+    dag=dag,
+    redshift_conn_id = "redshift",
+    tables = ["goodreads_analytics.popular_books_average_rating", "goodreads_analytics.popular_books_review_count"]
+
+)
+
+
+end_operator = EmptyOperator(task_id='Stop_execution',  dag=dag)
+
+start_operator >> jobOperator >> warehouse_data_quality_checks >> create_analytics_schema
+create_analytics_schema >> [create_author_analytics_table, create_book_analytics_table]
+create_author_analytics_table >> [load_author_table_reviews, load_author_table_ratings, load_best_author] >> authors_data_quality_checks
+create_book_analytics_table >> [load_book_table_reviews, load_book_table_ratings, load_best_book] >> books_data_quality_checks
+[authors_data_quality_checks, books_data_quality_checks] >> end_operator
---
+++ src/goodreads_transform.py
@@ -0,0 +1,162 @@
+from pyspark.sql.types import StringType
+from pyspark.sql import functions as fn
+import goodreads_udf
+import logging
+import configparser
+from pathlib import Path
+import os
+import sys
+import boto3
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+config = configparser.ConfigParser()
+config.read_file(open(f"{Path(__file__).parents[0]}/config.cfg"))
+
+os.environ["PYSPARK_PYTHON"] = sys.executable
+os.environ["PYSPARK_DRIVER_PYTHON"] = sys.executable
+
+class GoodreadsTransform:
+    """
+    This class performs transformation operations on the dataset.
+    1. Transform timestamp format, clean text part, remove extra spaces etc.
+    2. Create a lookup dataframe which contains the id and the timestamp for the latest record.
+    3. Join this lookup data frame with original dataframe to get only the latest records from the dataset.
+    4. Save the dataset by repartitioning. Using gzip compression
+    """
+
+    def __init__(self, spark):
+        self._spark = spark
+        self._load_path = config.get('BUCKET', 'WORKING_ZONE')
+        self._save_path = config.get('BUCKET', 'PROCESSED_ZONE')
+        self._session = boto3.Session(
+                            aws_access_key_id='AKIA47CRXYUC3K2IZINA',
+                            aws_secret_access_key='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'
+                        )
+
+    def transform_author_dataset(self):
+
+        logging.debug("Inside transform author dataset module")
+
+        s3 = self._session.resource('s3')
+        obj = s3.Object(self._load_path, 'author.csv')
+
+        df_pandas = pd.read_csv(obj.get()['Body'])
+
+        author_df = self._spark.createDataFrame(df_pandas)
+
+        author_lookup_df = author_df.groupby('author_id')\
+                            .agg(fn.max('record_create_timestamp').alias('record_create_timestamp'))
+        author_lookup_df.persist()
+        fn.broadcast(author_lookup_df)
+
+        deduped_author_df = author_df\
+                            .join(author_lookup_df, ['author_id', 'record_create_timestamp'], how='inner')\
+                            .select(author_df.columns) \
+                            .withColumn('name', goodreads_udf.remove_extra_spaces('name'))
+        
+        logging.debug(f"Attempting to write data to {self._save_path + '/author.csv'}")
+
+        filename = 'processed_author.csv'
+        author_df = deduped_author_df.toPandas()
+        author_df.to_csv(filename, index=False)
+
+        # Upload file to S3
+        s3.Object(self._save_path, filename).put(Body=open(filename, 'rb'))
+    
+
+    def transform_reviews_dataset(self):
+        logging.debug("Inside transform reviews dataset module")
+
+        s3 = self._session.resource('s3')
+        obj = s3.Object(self._load_path, 'reviews.csv')
+
+        df_pandas = pd.read_csv(obj.get()['Body'])
+
+        reviews_df = self._spark.createDataFrame(df_pandas)
+
+        reviews_lookup_df = reviews_df\
+                            .groupby('review_id')\
+                            .agg(fn.max('record_create_timestamp').alias('record_create_timestamp'))
+
+        reviews_lookup_df.persist()
+        fn.broadcast(reviews_lookup_df)
+
+        deduped_reviews_df = reviews_df \
+                             .join(reviews_lookup_df, ['review_id', 'record_create_timestamp'], how='inner')\
+                             .select(reviews_df.columns)
+
+        deduped_reviews_df = deduped_reviews_df \
+            .withColumn('review_added_date', goodreads_udf.stringtodatetime('review_added_date')) \
+            .withColumn('review_updated_date', goodreads_udf.stringtodatetime('review_updated_date'))
+
+
+        logging.debug(f"Attempting to write data to {self._save_path + '/reviews.csv'}")
+
+        filename = 'processed_reviews.csv'
+        reviews_df = deduped_reviews_df.toPandas()
+        reviews_df.to_csv(filename, index=False)
+
+        # Upload file to S3
+        s3.Object(self._save_path, filename).put(Body=open(filename, 'rb'))
+
+    def transform_books_dataset(self):
+        logging.debug("Inside transform books dataset module")
+        
+        s3 = self._session.resource('s3')
+        obj = s3.Object(self._load_path, 'book.csv')
+
+        df_pandas = pd.read_csv(obj.get()['Body'])
+        
+        books_df = self._spark.createDataFrame(df_pandas)
+
+        books_lookup_df = books_df\
+                            .groupby('book_id')\
+                            .agg(fn.max('record_create_timestamp').alias('record_create_timestamp'))
+        books_lookup_df.persist()
+        fn.broadcast(books_lookup_df)
+
+        deduped_books_df = books_df\
+                           .join(books_lookup_df, ['book_id', 'record_create_timestamp'], how='inner')\
+                           .select(books_df.columns)
+
+        logging.debug(f"Attempting to write data to {self._save_path + '/book.csv'}")
+
+        filename = 'processed_book.csv'
+        book_df = deduped_books_df.toPandas()
+        book_df.to_csv(filename, index=False)
+
+        # Upload file to S3
+        s3.Object(self._save_path, filename).put(Body=open(filename, 'rb'))
+
+
+    def tranform_users_dataset(self):
+        logging.debug("Inside transform users dataset module")
+        
+        s3 = self._session.resource('s3')
+        obj = s3.Object(self._load_path, 'user.csv')
+
+        df_pandas = pd.read_csv(obj.get()['Body'])
+        
+        users_df = self._spark.createDataFrame(df_pandas)
+
+        users_lookup_df = users_df\
+                          .groupby('user_id')\
+                           .agg(fn.max('record_create_timestamp').alias('record_create_timestamp'))
+
+        users_lookup_df.persist()
+        fn.broadcast(users_lookup_df)
+
+        deduped_users_df = users_df\
+                           .join(users_lookup_df, ['user_id', 'record_create_timestamp'], how='inner')\
+                           .select(users_df.columns)
+
+        logging.debug(f"Attempting to write data to {self._save_path + '/user.csv'}")
+
+        filename = 'processed_user.csv'
+        user_df = deduped_users_df.toPandas()
+        user_df.to_csv(filename, index=False)
+
+        # Upload file to S3
+        s3.Object(self._save_path, filename).put(Body=open(filename, 'rb'))
